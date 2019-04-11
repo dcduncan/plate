9c1a1594c4520d4d9f9c05e827c0f275cd02ad05
Daniel Duncan <dj0@fb.com>
2019-04-10 20:39:10 -0700

    Add initial request

--- src/app/App.js
+++ src/app/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+
+   state = {
+    loading: true,
+    response: null,
+   }
+
+   componentDidMount = () => {
+    Axios.get('http://localhost:8080/')
+    .then(response => this.setState({ response: response.data.response }))
+    .catch(error => this.setState({ response: error }))
+    .finally(() => this.setState({ loading: false }));
+   }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (this.state.loading) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+    }
+
+    return <h1>{this.state.response}</h1>;
   }
 }
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,6 @@
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CORS
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
@@ -7,11 +9,16 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.jetty.Jetty
 
 fun main(args: Array<String>) {
+
     embeddedServer(Jetty, 8080) {
+        install(CORS) {
+            host("localhost:3000")
+        }
         routing {
             get("/") {
-                call.respondText(ContentType.Application.Json) { """{"response":"root of plate"}""" }
+                call.respondText(ContentType.Application.Json) { """{"response":"root of le plate"}""" }
             }
         }
     }.start(wait = true)
+
 }
